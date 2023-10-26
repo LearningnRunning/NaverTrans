4935cf6cf4be7feab3592613fa5fe5791d8fbbd0
Rocki_Kim <rocki.k1208@gmail.com>
2023-10-26 10:55:43 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+required=[]
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+    for req in requirements:
+        p = req.split('==')
+        required.append(p[0])
+        
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()     
+setup(
+    name='navertrans',
+    version='0.0.2',
+    description= 'Thanks for NAVER',
+    long_description = long_description,
+    long_description_content_type='text/markdown',
+    author='learningnRunning',
+    author_email='max_sungrok@naver.com',
+    url='https://github.com/LearningnRunning/NaverTrans.git',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Education',
+        'Intended Audience :: End Users/Desktop',
+        'License :: Freeware',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Education',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    packages=find_packages(),
+    install_requires=required,
+)
 
     
 def install():
@@ -11,14 +46,13 @@ def install():
             p = req.split('==')
             required.append(p[0])
             
-    with open('README.md', encoding='utf-8') as f:
-        long_description = f.read()
+
         
     setup(
         name='navertrans',
-        version='0.0.1',
+        version='0.0.2',
         description= 'Thanks for NAVER',
-        long_description = long_description,
+        long_description = open('README.md').read(),
         long_description_content_type='text/markdown',
         author='learningnRunning',
         author_email='max_sungrok@naver.com',
